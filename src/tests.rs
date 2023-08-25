6bbffc62aac5a200df60b7ba4d6518501ad631ed
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-25 20:50:16 +0530

    done with tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,5 @@
+use super::structs::User;
+
 pub fn run_tests() {
     println!("Tests running");
 }
@@ -5,4 +7,38 @@ pub fn run_tests() {
 #[test]
 fn add_two() {
     assert_eq!(super::add_two(3), 5);
+}
+
+#[test] 
+fn contain_name() {
+    let user = User {
+        username: String::from("Wesley"),
+        email: String::from("wesley@gmail.com"),
+        sign_in_count: 10,
+        active: true,
+    };
+
+    assert!(user.contain_name());
+}
+
+#[test]
+fn does_not_contain_name() {
+    let user = User {
+        username: String::from("Lewis"),
+        email: String::from("lewis@gmail.com"),
+        sign_in_count: 5,
+        active: false,
+    };
+
+    assert_ne!(user.username, "myname", "The username should not be '{}'", "myname");
+}
+
+impl User {
+    pub fn contain_name(&self) -> bool {
+        if self.username != "" {
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
