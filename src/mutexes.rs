78ccec1d35d3b3b124fb6743f74f77fc3564c5c2
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-31 22:00:16 +0530

    mutexes in rust

--- src/mutexes.rs
+++ src/mutexes.rs
@@ -1,5 +1,6 @@
-use std::sync::Mutex;
 use std::thread;
+use std::rc::Rc;
+use std::sync::{Mutex, Arc};
 
 pub fn run_mutexes() {
     println!("Mutexes in rust"); 
@@ -10,13 +11,37 @@ pub fn run_mutexes() {
         *num = 6;
     }
     println!("m = {:?}", m);
+
+    atomic_references();
 }
 
-fn sharing_bw_multiple_threads() {
-    let counter = Mutex::new(0);
+//
+// fn sharing_bw_multiple_threads() {
+//     let counter = Rc::new(Mutex::new(0));
+//     let mut handles = vec![];
+//
+//     for _ in 0..10 {
+//         let counter = Rc::clone(&counter);
+//         let handle = thread::spawn(move || {
+//             let mut num = counter.lock().unwrap();
+//
+//             *num += 1;
+//         });
+//         handles.push(handle);
+//     }
+//
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+//     println!("Result: {}", *counter.lock().unwrap());
+// }
+
+fn atomic_references() {
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
