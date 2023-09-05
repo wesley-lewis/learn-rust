49f4acd5765f16535d8a94ae7ce5cb00ef292453
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-05 21:14:36 +0530

    advanced lifetimes started

--- src/advanced_lifetimes.rs
+++ src/advanced_lifetimes.rs
@@ -1,3 +1,25 @@
 pub fn run_lifetimes() {
     println!("Advanced Lifetimes");
+    let context = Context("Wesley Lewis");
+    let parser = Parser {context: &context };
+
+    println!("{:?}", parse_context(context).unwrap());
 }
+
+// lifetime subtyping
+struct Context<'s>(&'s str);
+
+struct Parser <'c, 's>{
+    context: &'c Context<'s>,
+}
+
+impl<'c, 's> Parser <'c, 's>{
+    fn parse(&self) -> Result<(), &'s str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
+// remaining
